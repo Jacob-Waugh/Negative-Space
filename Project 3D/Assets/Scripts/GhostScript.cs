611dfeac4343e43819a9ecd4abe97037ab2e7cca
notavirusgenerator <98986740+Milonster@users.noreply.github.com>
2023-11-20 12:45:55 -0500

    playerMask camera change

--- Project 3D/Assets/Scripts/GhostScript.cs
+++ Project 3D/Assets/Scripts/GhostScript.cs
@@ -18,7 +18,7 @@ public class GhostScript : MonoBehaviour
     {
         transform.LookAt(player.transform);
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, player.transform.position) < 0.001f)
+        if (Vector3.Distance(transform.position, player.transform.position) < 0.5f)
         {
             PlayersideCamera.instance.enemies.Remove(this.gameObject);
             Destroy(this.gameObject);
