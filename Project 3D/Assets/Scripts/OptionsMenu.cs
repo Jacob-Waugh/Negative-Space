0fdefdc2c0e5030373352883e278983767cd1427
LilyRead <147424140+LilyRead@users.noreply.github.com>
2023-11-30 16:17:36 -0500

    added models and changed options
    models and options menu

--- Project 3D/Assets/Scripts/OptionsMenu.cs
+++ Project 3D/Assets/Scripts/OptionsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -56,4 +57,9 @@ public class OptionsMenu : MonoBehaviour
  {
     Screen.fullScreen=isFullscreen;
  }
+
+ public void OnClickBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
